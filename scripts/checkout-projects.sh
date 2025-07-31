8945f2eb75c108d52e47d8212072fee08a16303d
Daniel Szekely <daniel@airtm.io>
2025-07-31 11:39:39 -0400

    pull other projects

--- scripts/checkout-projects.sh
+++ scripts/checkout-projects.sh
@@ -4,3 +4,11 @@ cd ~/projects
 if [[ ! -d budgeting ]]; then
     git clone https://github.com/Grim-Jokes/budgeting
 fi
+
+if [[ ! -d meal-planner ]]; then
+    git clone git@github.com:Grim-Jokes/meal-planner.git
+fi
+
+if [[ ! -d gantt-chart ]]; then
+    git clone git@github.com:Grim-Jokes/gantt-chart.git
+fi 
